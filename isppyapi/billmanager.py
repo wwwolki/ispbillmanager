fa7525acba43f9d8ffc8691e74064b7c08f6f685
Eugene Katsevman <eugene.katsevman@gmail.com>
2019-06-05 23:06:56 +0300

    new package folder

--- isppyapi/billmanager.py
+++ isppyapi/billmanager.py
@@ -128,39 +128,3 @@ class DnsManagerClient:
             result = await _handle_billmgr_response(response)
             return result
 
-async def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--user')
-    parser.add_argument('--password')
-    args = parser.parse_args()
-
-    session = aiohttp.ClientSession(connector=aiohttp.TCPConnector(verify_ssl=False))
-
-    config = configparser.ConfigParser()
-    config.read('config.ini')
-
-    async with session:
-        """
-        manager = BillManagerClient(session)
-        await manager.login(args.user, args.password)
-        result = await manager.list_vds()
-        for doc in result['doc']['elem']:
-            vds = VDS(**{key: doc[key]['$'] for key in VDS._field_types.keys()})
-            #print(vds)
-            print(doc)
-        """
-
-        manager = DnsManagerClient(session)
-        await manager.login(config['dnsmanager']['user'],
-                            config['dnsmanager']['password'])
-
-        result = await manager.list_domains()
-        pprint(result['doc']['elem'])
-        for elem in result['doc']['elem']:
-            name = elem['name']['$']
-            r = await manager.add_domain_record(name, 'google', '8.8.8.8')
-            pprint(r['doc'])
-
-
-loop = asyncio.get_event_loop()
-loop.run_until_complete(main())
---
+++ main.py
@@ -0,0 +1,42 @@
+import aiohttp
+import configparser
+
+from pprint import pprint
+from isppyapi import billmanager
+
+async def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--user')
+    parser.add_argument('--password')
+    args = parser.parse_args()
+
+    session = aiohttp.ClientSession(connector=aiohttp.TCPConnector(verify_ssl=False))
+
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+
+    async with session:
+        """
+        manager = BillManagerClient(session)
+        await manager.login(args.user, args.password)
+        result = await manager.list_vds()
+        for doc in result['doc']['elem']:
+            vds = VDS(**{key: doc[key]['$'] for key in VDS._field_types.keys()})
+            #print(vds)
+            print(doc)
+        """
+
+        manager = billmanager.DnsManagerClient(session)
+        await manager.login(config['dnsmanager']['user'],
+                            config['dnsmanager']['password'])
+
+        result = await manager.list_domains()
+        pprint(result['doc']['elem'])
+        for elem in result['doc']['elem']:
+            name = elem['name']['$']
+            r = await manager.add_domain_record(name, 'google', '8.8.8.8')
+            pprint(r['doc'])
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
