848092da6d1e0e0583176e45c3bd3d547da4a235
Eugene Katsevman <eugene.katsevman@gmail.com>
2019-06-05 23:24:46 +0300

    handle response moved to sep file

--- isppyapi/billmanager.py
+++ isppyapi/billmanager.py
@@ -2,20 +2,7 @@ from typing import NamedTuple
 
 import aiohttp
 
-
-async def _handle_billmgr_response(response: aiohttp.ClientResponse) -> dict:
-    if not response.status == 200:
-        raise ValueError("""Billmgr api returned bad status code: {}""".format(response.status_code))
-    try:
-        data = await response.json()
-    except ValueError:
-        raise ValueError("""Billmgr api returned an unknown response: {}""".format(response.text))
-    if "doc" not in data:
-        raise ValueError("""Billmgr api returned an unknown error""")
-    if "error" in data["doc"]:
-        raise ValueError("""Billmgr returned an error: {}""".format(data["doc"]["error"]["msg"]["$"]))
-    return data
-
+from isppyapi.helpers import _handle_billmgr_response
 
 VDS = NamedTuple('VDS', fields=[('createdate', str),
                                 ('domain', str),
