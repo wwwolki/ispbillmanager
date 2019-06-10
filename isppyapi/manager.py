f8a08d04fc55e76255513e97e303d0a87115eb65
eugene-katsevman <eugene.katsevman@gmail.com>
2019-06-10 23:45:58 +0300

    throw different exceptions on different errors (#2)

--- isppyapi/manager.py
+++ isppyapi/manager.py
@@ -3,6 +3,26 @@ from typing import Optional, Dict
 import aiohttp
 
 
+class BaseError(Exception):
+    """ Base exception for all managers """
+
+
+class BadCode(BaseError):
+    """ A wrapper for non-200 response """
+
+
+class UnknownError(BaseError):
+    """ Something nasty happened and we don't know what to do """
+    def __init__(self, text):
+        super().__init__("api returned an unknown error: {}".format(text))
+
+
+class ApiError(BaseError):
+    """ This is the only exception which should be handled and subclassed """
+    def __init__(self, error_description):
+        super().__init__('api returned error:{}'.format(error_description))
+
+
 class ManagerClient:
     def __init__(self,
                  session: Optional[aiohttp.ClientSession] = None,
@@ -14,14 +34,23 @@ class ManagerClient:
     async def _handle_response(self,
                                response: aiohttp.ClientResponse) -> Dict:
 
-        if not response.status == 200:
-            raise ValueError(self.__class__.__name__ + " api returned bad status code: {}".format(response.status_code))
+        """
+            https error codes are not used in this api, so we can safely assume
+            that something is very broken if we've got a non-200 response
+            for now we do not retry on  429 and 500s
+        """
+        try:
+            response.raise_for_status()
+        except aiohttp.ClientError as exc:
+            raise BadCode() from exc
+
         try:
             data = await response.json()
         except ValueError:
-            raise ValueError(self.__class__.__name__ + " api returned an unknown response: {}".format(response.text))
+            raise UnknownError(await response.text())
         if "doc" not in data:
-            raise ValueError(self.__class__.__name__ + " api returned an unknown error")
+            raise UnknownError(str(data))
         if "error" in data["doc"]:
-            raise ValueError(self.__class__.__name__ + " returned an error: {}".format(data["doc"]["error"]["msg"]["$"]))
-        return data
+            error = data["doc"]["error"]["msg"]["$"]
+            raise ApiError(error)
+        return data['doc']
